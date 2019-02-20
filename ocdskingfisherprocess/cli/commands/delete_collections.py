37b54c0baa89fd12a4ed4c7e32e113167b451ceb
James Baster <james.baster@opendataservices.coop>
2019-02-20 08:34:10 +0000

    delete_collections command - print output, Quiet Flag https://github.com/open-contracting/kingfisher-process/issues/21

--- ocdskingfisherprocess/cli/commands/delete_collections.py
+++ ocdskingfisherprocess/cli/commands/delete_collections.py
@@ -7,4 +7,6 @@ class DeleteCollectionsCLICommand(ocdskingfisherprocess.cli.commands.base.CLICom
     def run_command(self, args):
         for collection in self.database.get_all_collections():
             if collection.deleted_at is not None:
+                if not args.quiet:
+                    print("Collection " + str(collection.database_id))
                 self.database.delete_collection(collection.database_id)
