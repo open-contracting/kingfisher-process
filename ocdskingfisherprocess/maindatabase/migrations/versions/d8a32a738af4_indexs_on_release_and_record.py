94e54fc5b3263bbc55253b83eb89ec0e4f5f2f55
David Raznick <kindly@gmail.com>
2019-03-27 13:30:06 +0000

    add index to ocid column too

--- ocdskingfisherprocess/maindatabase/migrations/versions/d8a32a738af4_indexs_on_release_and_record.py
+++ ocdskingfisherprocess/maindatabase/migrations/versions/d8a32a738af4_indexs_on_release_and_record.py
@@ -18,7 +18,15 @@ def upgrade():
     op.create_index('release_collection_file_item_id_idx', 'release', ['collection_file_item_id'])
     op.create_index('record_collection_file_item_id_idx', 'record', ['collection_file_item_id'])
 
+    op.create_index('release_ocid_idx', 'release', ['ocid'])
+    op.create_index('record_ocid_idx', 'record', ['ocid'])
+    op.create_index('compiled_release_ocid_idx', 'compiled_release', ['ocid'])
+
 
 def downgrade():
     op.drop_index('release_collection_file_item_id_idx')
     op.drop_index('record_collection_file_item_id_idx')
+
+    op.drop_index('release_ocid_idx')
+    op.drop_index('record_ocid_idx')
+    op.drop_index('compiled_release_ocid_idx')
