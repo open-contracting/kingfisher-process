171ad993c9e3f29f5c8466b2ee489f88b88a54fe
David Raznick <kindly@gmail.com>
2019-03-27 09:44:34 +0000

    index collection_file_item_id in release/record table
    Makes transforms run much faster as they are joining from the
    collection_file_item table to the release/record tables (which are very
    large)

---
+++ ocdskingfisherprocess/maindatabase/migrations/versions/d8a32a738af4_index_on_release_and_record_file_item.py
@@ -0,0 +1,24 @@
+"""index on release and record file item
+
+Revision ID: d8a32a738af4
+Revises: 3811a084cc66
+Create Date: 2019-03-26 17:51:45.836678
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = 'd8a32a738af4'
+down_revision = '3811a084cc66'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_index('release_collection_file_item_id_idx', 'release', ['collection_file_item_id'])
+    op.create_index('record_collection_file_item_id_idx', 'record', ['collection_file_item_id'])
+
+
+def downgrade():
+    op.drop_index('release_collection_file_item_id_idx')
+    op.drop_index('record_collection_file_item_id_idx')
