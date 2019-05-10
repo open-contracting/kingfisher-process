0f4e68266d05917c856aa556029d73365c020e1c
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-05-10 14:35:45 -0400

    Fix version in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = '2018, Open Contracting Partnership'
 author = 'Open Contracting Partnership'
 
 # The short X.Y version
-version = '0.0.10'
+version = '0.0.1'
 # The full version, including alpha/beta/rc tags
 release = version
 
