09f8fd1e23cca24e2fcc2703f9351b2c52937511
James Baster <james.baster@opendataservices.coop>
2019-06-11 12:05:21 +0100

    Checks code - mark private methods with an underscore

--- ocdskingfisherprocess/checks.py
+++ ocdskingfisherprocess/checks.py
@@ -30,13 +30,13 @@ class Checks:
         # Normal Checks
         if self.collection.check_data:
 
-            self.process_releases(self.database.get_releases_to_check(self.collection.database_id))
+            self._process_releases(self.database.get_releases_to_check(self.collection.database_id))
 
             # Early return?
             if self.run_until_timestamp and self.run_until_timestamp < datetime.datetime.utcnow().timestamp():
                 return
 
-            self.process_records(self.database.get_records_to_check(self.collection.database_id))
+            self._process_records(self.database.get_records_to_check(self.collection.database_id))
 
             # Early return?
             if self.run_until_timestamp and self.run_until_timestamp < datetime.datetime.utcnow().timestamp():
@@ -45,7 +45,7 @@ class Checks:
         # Checks with schema V1.1
         if self.collection.check_older_data_with_schema_version_1_1:
 
-            self.process_releases_with_override_schema_version_1_1(
+            self._process_releases_with_override_schema_version_1_1(
                 self.database.get_releases_to_check(self.collection.database_id, override_schema_version="1.1")
             )
 
@@ -53,7 +53,7 @@ class Checks:
             if self.run_until_timestamp and self.run_until_timestamp < datetime.datetime.utcnow().timestamp():
                 return
 
-            self.process_records_with_override_schema_version_1_1(
+            self._process_records_with_override_schema_version_1_1(
                 self.database.get_records_to_check(self.collection.database_id, override_schema_version="1.1")
             )
 
@@ -72,7 +72,7 @@ class Checks:
                 .where(self.database.release_table.c.collection_file_item_id == collection_file_item_id)
             with self.database.get_engine().begin() as connection:
                 releases = connection.execute(s)
-            self.process_releases(releases)
+            self._process_releases(releases)
 
             # Early return?
             if self.run_until_timestamp and self.run_until_timestamp < datetime.datetime.utcnow().timestamp():
@@ -82,7 +82,7 @@ class Checks:
                 .where(self.database.record_table.c.collection_file_item_id == collection_file_item_id)
             with self.database.get_engine().begin() as connection:
                 records = connection.execute(s)
-            self.process_records(records)
+            self._process_records(records)
 
             # Early return?
             if self.run_until_timestamp and self.run_until_timestamp < datetime.datetime.utcnow().timestamp():
@@ -95,7 +95,7 @@ class Checks:
                 .where(self.database.release_table.c.collection_file_item_id == collection_file_item_id)
             with self.database.get_engine().begin() as connection:
                 releases = connection.execute(s)
-            self.process_releases_with_override_schema_version_1_1(releases)
+            self._process_releases_with_override_schema_version_1_1(releases)
 
             # Early return?
             if self.run_until_timestamp and self.run_until_timestamp < datetime.datetime.utcnow().timestamp():
@@ -105,51 +105,51 @@ class Checks:
                 .where(self.database.record_table.c.collection_file_item_id == collection_file_item_id)
             with self.database.get_engine().begin() as connection:
                 records = connection.execute(s)
-            self.process_records_with_override_schema_version_1_1(records)
+            self._process_records_with_override_schema_version_1_1(records)
 
             # Early return?
             if self.run_until_timestamp and self.run_until_timestamp < datetime.datetime.utcnow().timestamp():
                 return
 
-    def process_releases(self, releases):
+    def _process_releases(self, releases):
         for release_row in releases:
             # Do Normal Check?
             if not self.database.is_release_check_done(release_row['id']):
-                self.check_release_row(release_row)
+                self._check_release_row(release_row)
             # Early return?
             if self.run_until_timestamp and self.run_until_timestamp < datetime.datetime.utcnow().timestamp():
                 return
 
-    def process_records(self, records):
+    def _process_records(self, records):
         for record_row in records:
             # Do Normal Check?
             if not self.database.is_record_check_done(record_row['id']):
-                self.check_record_row(record_row)
+                self._check_record_row(record_row)
             # Early return?
             if self.run_until_timestamp and self.run_until_timestamp < datetime.datetime.utcnow().timestamp():
                 return
 
-    def process_releases_with_override_schema_version_1_1(self, releases):
+    def _process_releases_with_override_schema_version_1_1(self, releases):
         for release_row in releases:
             # Do 1.1 check?
-            if self.is_schema_version_less_than_1_1(release_row['package_data_id']) \
+            if self._is_schema_version_less_than_1_1(release_row['package_data_id']) \
                     and not self.database.is_release_check_done(release_row['id'], override_schema_version="1.1"):
-                self.check_release_row(release_row, override_schema_version="1.1")
+                self._check_release_row(release_row, override_schema_version="1.1")
             # Early return?
             if self.run_until_timestamp and self.run_until_timestamp < datetime.datetime.utcnow().timestamp():
                 return
 
-    def process_records_with_override_schema_version_1_1(self, records):
+    def _process_records_with_override_schema_version_1_1(self, records):
         for record_row in records:
             # Do 1.1 check?
-            if self.is_schema_version_less_than_1_1(record_row['package_data_id']) \
+            if self._is_schema_version_less_than_1_1(record_row['package_data_id']) \
                     and not self.database.is_record_check_done(record_row['id'], override_schema_version="1.1"):
-                self.check_record_row(record_row, override_schema_version="1.1")
+                self._check_record_row(record_row, override_schema_version="1.1")
             # Early return?
             if self.run_until_timestamp and self.run_until_timestamp < datetime.datetime.utcnow().timestamp():
                 return
 
-    def handle_package(self, package):
+    def _handle_package(self, package):
         cove_temp_folder = tempfile.mkdtemp(prefix='ocdskingfisher-cove-', dir=tempfile.gettempdir())
         try:
             return ocds_json_output(cove_temp_folder, None, None,
@@ -160,21 +160,21 @@ class Checks:
         finally:
             shutil.rmtree(cove_temp_folder)
 
-    def is_schema_version_less_than_1_1(self, package_data_id):
+    def _is_schema_version_less_than_1_1(self, package_data_id):
         # Performance wise, this is a bit dumb. We are basically calling get_package_data twice in a row!
         # We trust the database server will cache the result.
         # Later, maybe we'll want to see if improving this makes a big difference.
         data = self.database.get_package_data(package_data_id)
         return 'version' not in data or data['version'] == "1.0"
 
-    def check_release_row(self, release_row, override_schema_version=None):
+    def _check_release_row(self, release_row, override_schema_version=None):
         self.logger.debug('check_release_row called for row ' + str(release_row.id) + ' in collection ' + str(self.collection.database_id))
         package = self.database.get_package_data(release_row.package_data_id)
         package['releases'] = [self.database.get_data(release_row.data_id)]
         if override_schema_version:
             package['version'] = override_schema_version
         try:
-            cove_output = self.handle_package(package)
+            cove_output = self._handle_package(package)
             checks = [{
                 'release_id': release_row.id,
                 'cove_output': cove_output,
@@ -191,14 +191,14 @@ class Checks:
             with self.database.get_engine().begin() as connection:
                 connection.execute(self.database.release_check_error_table.insert(), checks)
 
-    def check_record_row(self, record_row, override_schema_version=None):
+    def _check_record_row(self, record_row, override_schema_version=None):
         self.logger.debug('check_record_row called for row ' + str(record_row.id) + ' in collection ' + str(self.collection.database_id))
         package = self.database.get_package_data(record_row.package_data_id)
         package['records'] = [self.database.get_data(record_row.data_id)]
         if override_schema_version:
             package['version'] = override_schema_version
         try:
-            cove_output = self.handle_package(package)
+            cove_output = self._handle_package(package)
             checks = [{
                 'record_id': record_row.id,
                 'cove_output': cove_output,
