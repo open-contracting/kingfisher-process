832b8af338b345dafcda49fe9e4eda92a63d568a
James Baster <james.baster@opendataservices.coop>
2019-08-02 15:49:22 +0100

    Delete Collections - add logging so we can debug speed issues

--- ocdskingfisherprocess/cli/commands/delete_collections.py
+++ ocdskingfisherprocess/cli/commands/delete_collections.py
@@ -1,12 +1,17 @@
 import ocdskingfisherprocess.cli.commands.base
+import logging
 
 
 class DeleteCollectionsCLICommand(ocdskingfisherprocess.cli.commands.base.CLICommand):
     command = 'delete-collections'
 
     def run_command(self, args):
+        logger = logging.getLogger('ocdskingfisher.cli.delete-collections')
+        logger.info("Starting command")
         for collection in self.database.get_all_collections():
             if collection.deleted_at is not None:
                 if not args.quiet:
                     print("Collection " + str(collection.database_id))
+                logger.info("Starting to delete collection " + str(collection.database_id))
                 self.database.delete_collection(collection.database_id)
+        logger.info("Finishing command")
