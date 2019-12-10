7ce617f911a03bc6a51546f04e05d23eeda6c83d
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-12-10 17:47:41 -0500

    tests: Use inheritance instead of repeating code

--- tests/base.py
+++ tests/base.py
@@ -10,54 +10,43 @@ def _reset_signals():
 
 
 class BaseTest:
-
     def alter_config(self):
+        """
+        Implement this method in a subclass to edit ``self.config``.
+        """
         pass
 
+    # https://docs.pytest.org/en/latest/xunit_setup.html#method-and-function-level-setup-teardown
     def setup_method(self, test_method):
-        # config
         self.config = Config()
         self.config.load_user_config()
         self.alter_config()
 
 
-class BaseDataBaseTest:
-
-    def alter_config(self):
-        pass
-
+class AbstractDataBaseTest(BaseTest):
     def setup_method(self, test_method):
-        # config
-        self.config = Config()
-        self.config.load_user_config()
-        self.alter_config()
-        # database
+        super().setup_method(test_method)
+
         self.database = DataBase(config=self.config)
         self.database.delete_tables()
         self.database.create_tables()
-        # signals
+
+
+class BaseDataBaseTest(AbstractDataBaseTest):
+    def setup_method(self, test_method):
+        super().setup_method(test_method)
+
         _reset_signals()
         setup_signals(self.config, self.database)
 
 
-class BaseWebTest:
-
-    def alter_config(self):
-        pass
-
+class BaseWebTest(AbstractDataBaseTest):
     def setup_method(self, test_method):
-        # config
-        self.config = Config()
-        self.config.load_user_config()
-        self.alter_config()
-        # database
-        self.database = DataBase(config=self.config)
-        self.database.delete_tables()
-        self.database.create_tables()
-        # signals
+        super().setup_method(test_method)
+
         _reset_signals()
         # don't call - setup_signals(self.config, self.database) - create_app will
-        # flask app
+
         self.webapp = create_app(config=self.config)
         self.webapp.config['TESTING'] = True
         self.flaskclient = self.webapp.test_client()
