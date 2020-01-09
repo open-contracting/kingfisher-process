8cfaba784327bab95de1260921d1cd069d0a5aaa
James Baster <james.baster@opendataservices.coop>
2020-01-09 08:32:31 +0000

    Add collection id to release, record, compiled_release: Stage 1
    Add columns and indexes, NULLABLE
    
    https://github.com/open-contracting/kingfisher-process/issues/236

---
+++ ocdskingfisherprocess/maindatabase/migrations/versions/bdfc23d8334c_add_collection_id.py
@@ -0,0 +1,43 @@
+"""add-collection-id
+
+Revision ID: bdfc23d8334c
+Revises: 0fa56a3d3f90
+Create Date: 2020-01-09 08:30:01.478751
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'bdfc23d8334c'
+down_revision = '0fa56a3d3f90'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        'record',
+        sa.Column('collection_id', sa.Integer,
+                  sa.ForeignKey('collection.id', name='fk_record_collection_id'),
+                  nullable=True))
+    op.add_column(
+        'release',
+        sa.Column('collection_id', sa.Integer,
+                  sa.ForeignKey('collection.id', name='fk_release_collection_id'),
+                  nullable=True))
+    op.add_column(
+        'compiled_release',
+        sa.Column('collection_id', sa.Integer,
+                  sa.ForeignKey('collection.id', name='fk_compiled_release_collection_id'),
+                  nullable=True))
+
+    op.create_index('record_collection_id_idx', 'record', ['collection_id'])
+    op.create_index('release_collection_id_idx', 'release', ['collection_id'])
+    op.create_index('compiled_release_collection_id_idx', 'compiled_release', ['collection_id'])
+
+
+def downgrade():
+    op.drop_column('record', 'collection_id')
+    op.drop_column('release', 'collection_id')
+    op.drop_column('compiled_release', 'collection_id')
