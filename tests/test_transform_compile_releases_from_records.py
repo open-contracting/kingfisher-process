ab8c07c57fc26651f0ce24975d12cdc22e0d0230
James <james.baster@opendataservices.coop>
2020-04-23 11:32:55 +0100

    transform: Compile releases - if releases with no dates, compile releases without dates and warn
    https://github.com/open-contracting/kingfisher-process/issues/279

--- tests/test_transform_compile_releases_from_records.py
+++ tests/test_transform_compile_releases_from_records.py
@@ -115,8 +115,8 @@ class TestTransformCompileReleasesFromRecords(BaseDataBaseTest):
         notes = self.database.get_all_notes_in_collection(destination_collection_id)
         assert len(notes) == 1
         assert 'OCID ocds-213czf-000-00001 could not be compiled because at least one release in the releases ' +\
-               'array is a linked release, and the record has neither a compileRelease ' +\
-               'nor a release with a tag of "compiled".' == \
+               'array is a linked release or there are no releases with dates, ' +\
+               'and the record has neither a compileRelease nor a release with a tag of "compiled".' == \
                notes[0].note
 
     def test_transform_compiles(self):
@@ -215,7 +215,7 @@ class TestTransformCompileReleasesFromRecords(BaseDataBaseTest):
     def test_no_dates(self):
 
         source_collection_id, source_collection, destination_collection_id, destination_collection = \
-            self._setup_collections_and_data_run_transform('sample_1_1_record_releases_not_compiled_no_date.json')
+            self._setup_collections_and_data_run_transform('sample_1_1_record_releases_not_compiled_no_dates.json')
 
         # check
         with self.database.get_engine().begin() as connection:
@@ -226,6 +226,40 @@ class TestTransformCompileReleasesFromRecords(BaseDataBaseTest):
         # Check collection notes
         notes = self.database.get_all_notes_in_collection(destination_collection_id)
         assert len(notes) == 1
-        assert 'OCID ocds-213czf-000-00001 could not be compiled because merge library threw an error: ' + \
-               'MissingDateKeyError The `date` field of at least one release is missing.' == \
+        assert 'OCID ocds-213czf-000-00001 could not be compiled ' +\
+               'because at least one release in the releases array is a ' +\
+               'linked release or there are no releases with dates, ' +\
+               'and the record has neither a compileRelease nor a release with a tag of "compiled".' == \
                notes[0].note
+
+    def test_some_dates(self):
+
+        source_collection_id, source_collection, destination_collection_id, destination_collection = \
+            self._setup_collections_and_data_run_transform('sample_1_1_record_releases_not_compiled_some_dates.json')
+
+        # check
+        with self.database.get_engine().begin() as connection:
+            s = sa.sql.select([self.database.compiled_release_table])
+            result = connection.execute(s)
+            assert 1 == result.rowcount
+
+            # Check a couple of fields just to sanity check it's the compiled release in the record table
+            # Because releases are linked, the only way to get data is to take the compiled release
+            compiled_release = result.fetchone()
+            data = self.database.get_data(compiled_release['data_id'])
+            assert 'ocds-213czf-000-00001-2011-01-10T09:30:00Z' == data.get('id')
+            assert '2011-01-10T09:30:00Z' == data.get('date')
+            assert 'ocds-213czf-000-00001' == data.get('ocid')
+
+            # Check warnings
+            s = sa.sql.select([self.database.collection_file_item_table]) \
+                .where(self.database.collection_file_item_table.c.id == compiled_release['collection_file_item_id'])
+            result_file_item = connection.execute(s)
+            assert 1 == result_file_item.rowcount
+            collection_file_item = result_file_item.fetchone()
+            assert collection_file_item.warnings == [
+                'This OCID had some releases without a date element. We have compiled all other releases.']
+
+        # Check collection notes
+        notes = self.database.get_all_notes_in_collection(destination_collection_id)
+        assert len(notes) == 0
