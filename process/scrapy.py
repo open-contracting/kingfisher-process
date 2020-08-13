a37fa988ebd4eb5c6297e8e32aa2ba663a1e91d5
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-08-13 13:01:47 -0400

    Add scrapy module

---
+++ process/scrapy.py
@@ -0,0 +1,16 @@
+from urllib.parse import urljoin
+
+import requests
+from django.conf import settings
+
+
+def spiders():
+    """
+    :returns: the names of the spiders in the Scrapyd project
+    :rtype: list
+    """
+    # https://scrapyd.readthedocs.io/en/stable/api.html#listspiders-json
+    url = urljoin(settings.SCRAPYD['url'], '/listspiders.json?project=' + settings.SCRAPYD['project'])
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()['spiders']
