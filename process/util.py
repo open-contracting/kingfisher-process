9dc36bc767f97d6f0011a60948b81441d77e2b4f
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-08-16 11:15:39 -0400

    Add walk utility function

--- process/util.py
+++ process/util.py
@@ -1,3 +1,4 @@
+import os
 from textwrap import fill
 
 import orjson
@@ -15,3 +16,14 @@ def wrap(string):
     Formats a long string as a help message, and returns it.
     """
     return '\n\n'.join(fill(paragraph, width=78, replace_whitespace=False) for paragraph in string.splitlines())
+
+
+def walk(paths):
+    for path in paths:
+        if os.path.isfile(path):
+            yield path
+        else:
+            for root, dirs, files in os.walk(path):
+                for name in files:
+                    if not name.startswith('.'):
+                        yield os.path.join(root, name)
