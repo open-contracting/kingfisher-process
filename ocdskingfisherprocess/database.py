84448544367dbc56d69e0368bb6801dad907d88f
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-11-24 12:14:04 -0500

    flake8

--- ocdskingfisherprocess/database.py
+++ ocdskingfisherprocess/database.py
@@ -701,8 +701,14 @@ class DataBase:
             "collection_file", "DELETE FROM collection_file WHERE collection_id = :collection_id;", collection_id)
         self._delete_collection_run_sql(
             "collection_note", "DELETE FROM collection_note WHERE collection_id = :collection_id;", collection_id)
-        self._delete_collection_run_sql("collection", "UPDATE collection SET transform_from_collection_id = NULL "
-            "WHERE transform_from_collection_id = :collection_id;", collection_id)
+        self._delete_collection_run_sql(
+            "collection",
+            """
+                UPDATE collection
+                SET transform_from_collection_id = NULL
+                WHERE transform_from_collection_id = :collection_id;
+            """,
+            collection_id)
         self._delete_collection_run_sql(
             "collection", "DELETE FROM collection WHERE id = :collection_id;", collection_id)
 
