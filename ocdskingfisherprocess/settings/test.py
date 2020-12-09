1934faabe9bc6288ec7d19b111527a7a24351ca3
Kuba Krafka <jakub@krafka.net>
2020-12-09 09:38:43 +0100

    updated settings

--- ocdskingfisherprocess/settings/test.py
+++ ocdskingfisherprocess/settings/test.py
@@ -22,11 +22,11 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'kingfisher',                      # Or path to database file if using sqlite3.
+        'NAME': 'kingfisher-process',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': 'kingfisher',                  # Not used with sqlite3.
+        'PASSWORD': 'postgres',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '22060',                      # Set to empty string for default. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
