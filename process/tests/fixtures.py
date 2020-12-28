a4298f9f3f646fa53c551fef44e4c856c04480fb
Kuba Krafka <jakub@krafka.net>
2020-12-28 15:45:35 +0100

    added processor

--- process/tests/fixtures.py
+++ process/tests/fixtures.py
@@ -3,8 +3,5 @@ from process.models import Collection
 
 def collection(**kwargs):
     return Collection(
-        source_id='france',
-        data_version='2001-01-01 00:00:00',
-        store_start_at='2001-01-01 00:00:00',
-        **kwargs
+        id=1, source_id="france", data_version="2001-01-01 00:00:00", store_start_at="2001-01-01 00:00:00", **kwargs
     )
