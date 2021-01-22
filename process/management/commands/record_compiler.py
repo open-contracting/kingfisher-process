8c0a76cabe4111b2b0e9735d639dbda1692d540d
Kuba Krafka <jakub@krafka.net>
2021-01-22 15:02:23 +0100

    basic version of compiler

---
+++ process/management/commands/record_compiler.py
@@ -0,0 +1,52 @@
+import json
+import sys
+
+from django.db import transaction
+
+from process.management.commands.base.worker import BaseWorker
+from process.processors.compiler import compile_record
+
+
+class Command(BaseWorker):
+    """
+    The worker is responsible for the compilation of particular records.
+    Consumes messages with an ocid and collection_id which should be compiled.
+    The whole structure of CollectionFile, CollectionFileItem, and CompiledRelease
+    is created and saved.
+    It's safe to run multiple workers of this type at the same type.
+    """
+
+    worker_name = "record_compiler"
+
+    consume_keys = ["compiler_record"]
+
+    def __init__(self):
+        super().__init__(self.worker_name)
+
+    def process(self, channel, method, properties, body):
+        try:
+            # parse input message
+            input_message = json.loads(body.decode("utf8"))
+
+            self.debug("Received message {}".format(input_message))
+
+            ocid = input_message["ocid"]
+            collection_id = input_message["collection_id"]
+
+            with transaction.atomic():
+                self.info("Compiling release collection_id: {} ocid: {}".format(collection_id, ocid))
+                release = compile_record(collection_id, ocid)
+
+            # publish message about processed item
+            message = {
+                "ocid": ocid,
+                "compiled_release_id": release.pk,
+            }
+
+            self.publish(json.dumps(message))
+
+            channel.basic_ack(delivery_tag=method.delivery_tag)
+        except Exception:
+            channel.basic_nack(delivery_tag=method.delivery_tag)
+            self.exception("Something went wrong when processing {}".format(body))
+            sys.exit(1)
--- process/processors/compiler.py
+++ process/processors/compiler.py
@@ -10,6 +10,7 @@ from process.models import (
     CollectionFileStep,
     CompiledRelease,
     Data,
+    Record,
     Release,
 )
 from process.util import get_hash
@@ -83,8 +84,79 @@ def compile_release(collection_id, ocid):
 
     # merge data into into single compiled release
     merger = Merger()
-    compiled_release = merger.create_compiled_release(releases_data)
+    compiled_release_data = merger.create_compiled_release(releases_data)
 
+    return _save_compiled_release(compiled_release_data, collection, ocid)
+
+
+def compile_record(collection_id, ocid):
+    """
+    Compiles records of given ocid in a "parent" collection. Creates the whole structure in the "proper" transformed
+    collection - CollectionFile, CollectionFileItems, Data, and CompiledRelease.
+
+    :param int collection_id: collection id to which the ocid belongs to
+    :param str ocid: ocid of release whiich should be compiled
+
+    :returns: compiled release
+    :rtype: CompiledRelease
+
+    :raises TypeError: if there arent integers provided on input
+    :raises ValueError: if there are no items of such id (collections/ocid)
+    :raises ValueError: if the compiled release already exists
+    :raises ValueError: if there are no records to compile in the source/parent collection
+    """
+
+    # validate input
+    if not isinstance(collection_id, int):
+        raise TypeError("collection_id is not an int value")
+    if not isinstance(ocid, str):
+        raise TypeError("ocid is not a string value")
+
+    logger.info("Compiling record collection_id: {} ocid: {}".format(collection_id, ocid))
+
+    # retrieve collection including its parent from db
+    try:
+        collection = Collection.objects.filter(parent=collection_id).get(parent__steps__contains="compile")
+        if not collection:
+            # no collection found
+            raise ValueError("Compiled collection with parent collection id {} not found".format(collection_id))
+    except Collection.DoesNotExist:
+        raise ValueError("Compiled collection with parent collection id {} not found".format(collection_id))
+
+    # check, whether the ocid wasnt already compiled
+    try:
+        compiled_release = CompiledRelease.objects.filter(ocid__exact=ocid).get(
+            collection_file_item__collection_file__collection=collection
+        )
+        raise AlreadyExists(
+            "CompiledRelease {} for a collection {} already exists".format(compiled_release, collection)
+        )
+    except CompiledRelease.DoesNotExist:
+        # happy day - compiled release should not exist
+        logger.debug("compiled_release with ocid {} does not exist, we can compile that and store it".format(ocid))
+
+    # get records for given ocid
+    try:
+        record = (
+            Record.objects.filter(collection_file_item__collection_file__collection=collection.parent)
+            .prefetch_related("data")
+            .get(ocid=ocid)
+        )
+    except Record.DoesNotExist:
+        raise ValueError("No records with ocid {} found in parent collection.".format(ocid))
+
+    # create array with all the data for releases
+    records_data = []
+    for release in releases:
+        releases_data.append(release.data.data)
+
+    # merge data into into single compiled release
+    merger = Merger()
+    compiled_release_data = merger.create_compiled_release(releases_data)
+    return _save_compiled_release(compiled_release_data, collection, ocid)
+
+
+def _save_compiled_release(compiled_release_data, collection, ocid):
     # create new collection file
     compiled_collection_file = CollectionFile()
     compiled_collection_file.collection = collection
@@ -100,7 +172,7 @@ def compile_release(collection_id, ocid):
     logger.debug("Created new collection_file_item {}".format(collection_file_item))
 
     # calculate hash to retrieve data
-    compiled_release_hash = get_hash(str(compiled_release))
+    compiled_release_hash = get_hash(str(compiled_release_data))
 
     try:
         # is this item already saved?
@@ -109,7 +181,7 @@ def compile_release(collection_id, ocid):
     except (Data.DoesNotExist, Data.MultipleObjectsReturned):
         # not saved yet, create new one
         data = Data()
-        data.data = compiled_release
+        data.data = compiled_release_data
         data.hash_md5 = compiled_release_hash
         data.save()
         logger.debug("Created new data item {}".format(data))
