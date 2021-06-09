df8681c82f206dff110ed83a20ed004f83c7e67d
Kuba Krafka <jakub@krafka.net>
2021-06-09 11:12:23 +0200

    dockerized

--- gunicorn_runner.sh
+++ gunicorn_runner.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn --bind 0.0.0.0:8002 ocdskingfisherprocess.amy01_wsgi -c gunicorn_amy01.py
+gunicorn --bind 0.0.0.0:8000 ocdskingfisherprocess.amy01_wsgi -c gunicorn_amy01.conf.py
