33697f773c39f0eadf708ece144686298bf2e88d
Kuba Krafka <jakub@krafka.net>
2021-08-05 11:52:16 +0200

    fix

--- process/management/commands/base/worker.py
+++ process/management/commands/base/worker.py
@@ -91,7 +91,7 @@ class BaseWorker(BaseCommand):
         else:
             publish_routing_key = self.rabbit_publish_routing_key
 
-        if not self.rabbit_channel.is_open():
+        if not self.rabbit_channel.is_open:
             # recover timed out connections
             self._warning("Rabbit connection timeout, reinit.")
             self._initMessaging()
