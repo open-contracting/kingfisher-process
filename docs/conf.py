29868a4fba433f49ffe7baa7781f2ae00b39b208
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-12-14 14:50:48 -0500

    docs: Use furo

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "default"
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -60,18 +60,8 @@ html_static_path = []
 
 # -- Extension configuration -------------------------------------------------
 
-# Needed for ReadTheDocs (Sphinx 1.8).
-master_doc = "index"
-
 autodoc_default_options = {
     "members": None,
+    "member-order": "bysource",
 }
-autodoc_member_order = "bysource"
-
-on_rtd = os.environ.get("READTHEDOCS", None) == "True"
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-
-    html_theme = "sphinx_rtd_theme"
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+autodoc_typehints = "description"
