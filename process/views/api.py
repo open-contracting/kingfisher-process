6a145e9770688d512e4e0774cc4a9e8d7d82468c
jakubkrafka <jakub@krafka.net>
2022-01-11 08:27:51 +0100

    Update process/views/api.py
    Co-authored-by: James McKinney <26463+jpmckinney@users.noreply.github.com>

--- process/views/api.py
+++ process/views/api.py
@@ -27,7 +27,7 @@ def create_collection(request):
             )
 
         try:
-            if not settings.ENABLE_CHECKER and input_message.get("check", False):
+            if not settings.ENABLE_CHECKER and input_message.get("check"):
                 logger.error("Checker is disabled in settings - see ENABLE_CHECKER value.")
 
             collection, upgraded_collection, compiled_collection = create_collections(
