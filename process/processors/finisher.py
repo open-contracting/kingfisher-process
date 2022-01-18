6128bbe3614355af3f23bfe72fbf67855e656a5d
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-01-18 17:12:26 -0500

    chore: Avoid unnecessary if-condition nesting in completable()

--- process/processors/finisher.py
+++ process/processors/finisher.py
@@ -20,52 +20,43 @@ def completable(collection_id):
 
     try:
         collection = Collection.objects.get(pk=collection_id)
-
-        if collection.completed_at:
-            # already marked as completed in a past
-            logger.warning("Collection %s already marked as completed", collection)
-            return False
-
-        if collection.store_end_at is not None or (
-            collection.store_end_at is None
-            and collection.transform_type == Collection.Transforms.COMPILE_RELEASES
-            and collection.get_root_parent().store_end_at is not None
-        ):
-
-            if (
-                collection.transform_type == Collection.Transforms.COMPILE_RELEASES
-                and not collection.compilation_started
-            ):
-
-                # special case when the collection should be compiled and
-                # waits for compilation to be planned
-                # in such case, no processing steps will be available yet
-                logger.debug("Compilation of collection %s not started yet", collection)
-
-                return False
-
-            has_steps_remaining = ProcessingStep.objects.filter(collection=collection).exists()
-            if not has_steps_remaining:
-                real_files_count = CollectionFile.objects.filter(collection=collection).count()
-                if collection.expected_files_count and collection.expected_files_count > real_files_count:
-                    logger.debug(
-                        "Collection %s is not completable yet. There are (probably) some"
-                        "unprocessed messages in the queue with the new items"
-                        " - expected files count %s real files count %s",
-                        collection,
-                        collection.expected_files_count,
-                        real_files_count,
-                    )
-                    return False
-
-                return True
-            else:
-                logger.debug("Processing not finished yet for collection %s - >= 1 remaining steps.", collection)
-                return False
-        else:
-            logger.debug("Collection %s not completely stored yet.", collection)
-            return False
-
     except Collection.DoesNotExist:
-        logger.info("Collection id %s not found", collection_id)
+        logger.warning("Collection %s not completable (not found)", collection_id)
         return False
+
+    if collection.completed_at:
+        logger.warning("Collection %s not completable (already completed)", collection)
+        return False
+
+    # compile-releases collections don't set the `store_end_at` field - instead check the root collection.
+    if collection.store_end_at is None and (
+        collection.transform_type != Collection.Transforms.COMPILE_RELEASES
+        or collection.get_root_parent().store_end_at is None
+    ):
+        logger.debug("Collection %s not completable (load not finished)", collection)
+        return False
+
+    # special case when the collection should be compiled and
+    # waits for compilation to be planned
+    # in such case, no processing steps will be available yet
+    if collection.transform_type == Collection.Transforms.COMPILE_RELEASES and not collection.compilation_started:
+        logger.debug("Collection %s not completable (compile not started)", collection)
+        return False
+
+    has_steps_remaining = ProcessingStep.objects.filter(collection=collection).exists()
+    if has_steps_remaining:
+        logger.debug("Collection %s not completable (steps remaining)", collection)
+        return False
+
+    real_files_count = CollectionFile.objects.filter(collection=collection).count()
+    if collection.expected_files_count and collection.expected_files_count > real_files_count:
+        logger.debug(
+            "Collection %s not completable. There are (probably) some unprocessed messages in the queue with the new "
+            "items - expected files count %s, real files count %s",
+            collection,
+            collection.expected_files_count,
+            real_files_count,
+        )
+        return False
+
+    return True
