1f4a1d9626ef81562b8610235db90ef568a02018
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-01-27 13:47:48 -0500

    chore: The message never sets a "close" key

--- process/management/commands/api_loader.py
+++ process/management/commands/api_loader.py
@@ -3,7 +3,6 @@ import os.path
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import transaction
-from django.db.models.functions import Now
 from yapw.methods.blocking import ack, publish
 
 from process.models import Collection
@@ -34,16 +33,6 @@ def callback(client_state, channel, method, properties, input_message):
             url=input_message.get("url"),
             errors=input_message.get("errors"),
         )
-
-        if input_message.get("close"):
-            collection = Collection.objects.select_for_update().get(pk=collection_id)
-            collection.store_end_at = Now()
-            collection.save()
-
-            upgraded_collection = collection.get_upgraded_collection()
-            if upgraded_collection:
-                upgraded_collection.store_end_at = Now()
-                upgraded_collection.save()
 
     # FileError items from Kingfisher Collect are not processed further.
     if "errors" not in input_message:
