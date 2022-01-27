c1c1724d43e9ad2ca6061ec11dc0bd10aee910f7
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-01-26 20:09:51 -0500

    fix: Set collection_id in loader messages, like for api_loader messages in  dba6eb967f3e4bcf4c4d376331d85d842b238b70

--- process/management/commands/add_files.py
+++ process/management/commands/add_files.py
@@ -59,6 +59,7 @@ class Command(BaseCommand):
                 with transaction.atomic():
                     logger.debug("Storing file %s", file_path)
                     collection_file = create_collection_file(collection, file_path)
-                    client.publish({"collection_file_id": collection_file.pk}, routing_key=routing_key)
+                    message = {"collection_id": collection.pk, "collection_file_id": collection_file.pk}
+                    client.publish(message, routing_key=routing_key)
 
         logger.info("Load command completed")
