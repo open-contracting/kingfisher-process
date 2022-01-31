676d8577257532aaf476dedcbb23879c076ed76d
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-01-31 15:52:41 -0500

    chore: Use CollectionCommand in add_files

--- process/management/commands/add_files.py
+++ process/management/commands/add_files.py
@@ -1,64 +1,50 @@
-import logging
-import os
-
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import CommandError
 from django.db import transaction
 from django.utils.translation import gettext as t
 from django.utils.translation import gettext_lazy as _
 
-from process.models import Collection
+from process.cli import CollectionCommand
 from process.processors.loader import create_collection_file, file_or_directory
 from process.util import get_publisher, walk
 from process.util import wrap as w
 
-logger = logging.getLogger(__name__)
 routing_key = "loader"
 
 
-class Command(BaseCommand):
-    help = w(t("Load data into a (open) collection, asynchronously"))
+class Command(CollectionCommand):
+    help = w(t("Load data into an open root collection, asynchronously"))
 
-    def add_arguments(self, parser):
-        parser.add_argument("PATH", help=_("a file or directory to load"), nargs="+", type=file_or_directory)
-        parser.add_argument(
-            "-c",
-            "--collection",
-            help=_("the id of collection to which the files whould be loaded"),
-        )
+    def add_collection_arguments(self, parser):
+        parser.add_argument("path", help=_("the file or directory to load"), nargs="+", type=file_or_directory)
 
-    def handle(self, *args, **options):
-        if not options["collection"]:
-            raise CommandError(_("Please indicate collection by its id to load data at."))
-
-        try:
-            collection_id = int(options["collection"])
-        except ValueError:
-            raise CommandError(_("--collection %(id)s is not an int value") % {"id": options["collection"]})
-
-        # check whether data source exists
-        mtimes = [os.path.getmtime(path) for path in walk(options["PATH"])]
-        if not mtimes:
-            raise CommandError(_("No files found"))
-
-        try:
-            collection = Collection.objects.get(pk=collection_id)
-        except Collection.DoesNotExist:
-            raise CommandError(_("Collection id=%(id)s not found") % {"id": collection_id})
-
+    def handle_collection(self, collection, *args, **options):
         if collection.store_end_at:
-            raise CommandError(_("Collection id=%(id)s already closed at %(store_end_at)s") % collection.__dict__)
+            raise CommandError(
+                _("Collection %(id)s is not an open collection. It was closed at %(store_end_at)s.")
+                % collection.__dict__
+            )
 
-        logger.debug("Processing path %s", options["PATH"])
+        if collection.parent_id:
+            raise CommandError(
+                _("Collection %(id)s is not a root collection. Its parent is collection %(parent_id)s.")
+                % collection.__dict__
+            )
+
+        try:
+            next(walk(options["path"]))
+        except StopIteration:
+            raise CommandError(_("No files to load"))
 
         with get_publisher() as client:
-            for file_path in walk(options["PATH"]):
+            for path in walk(options["path"]):
+                print(f"Adding {path}")
+
                 # The transaction is inside the loop, since we can't rollback RabbitMQ messages, only PostgreSQL
                 # statements. This ensures that any published message is paired with a database commit.
                 with transaction.atomic():
-                    logger.debug("Storing file %s", file_path)
-                    collection_file = create_collection_file(collection, file_path)
+                    collection_file = create_collection_file(collection, path)
 
                 message = {"collection_id": collection.pk, "collection_file_id": collection_file.pk}
                 client.publish(message, routing_key=routing_key)
 
-        logger.info("Load command completed")
+        print("done")
