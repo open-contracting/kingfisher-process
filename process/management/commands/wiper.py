0fa1ae0fd2c2c50ab8a7b94a528d839fdd68f146
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-02-07 13:21:41 -0500

    fix: Allow duplicate messages for wiper

--- process/management/commands/wiper.py
+++ process/management/commands/wiper.py
@@ -1,11 +1,15 @@
+import logging
+
 from django.core.management.base import BaseCommand
-from yapw.methods.blocking import ack
+from yapw.methods.blocking import ack, nack
 
 from process.models import Collection
 from process.util import consume, decorator
 
 consume_routing_keys = ["wiper"]
 routing_key = "wiper"
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -16,6 +20,10 @@ class Command(BaseCommand):
 def callback(client_state, channel, method, properties, input_message):
     collection_id = input_message["collection_id"]
 
-    Collection.objects.get(pk=collection_id).delete()
-
-    ack(client_state, channel, method.delivery_tag)
+    try:
+        Collection.objects.get(pk=collection_id).delete()
+    except Collection.DoesNotExist as exception:
+        logger.error(f"{exception.__class__.__name__} possibly caused by duplicate message: {exception}")
+        nack(client_state, channel, method.delivery_tag, requeue=False)
+    else:
+        ack(client_state, channel, method.delivery_tag)
