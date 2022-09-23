e00e1b9d5e2d10bdd70215c5d84476eb5a7a79f6
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-09-23 16:55:43 -0400

    fix: Attempt fix for OOM issues when deleting large collections (using types.MethodType to bind correctly)

--- process/management/commands/wiper.py
+++ process/management/commands/wiper.py
@@ -1,6 +1,8 @@
 import logging
+import types
 
 from django.core.management.base import BaseCommand
+from django.db import connections
 from yapw.methods.blocking import ack
 
 from process.models import Collection
@@ -12,6 +14,10 @@ routing_key = "wiper"
 logger = logging.getLogger(__name__)
 
 
+def bulk_batch_size(self, fields, objs):
+    return 65536  # 2**16
+
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         consume(callback, routing_key, consume_routing_keys, decorator=decorator)
@@ -19,6 +25,13 @@ class Command(BaseCommand):
 
 def callback(client_state, channel, method, properties, input_message):
     collection_id = input_message["collection_id"]
+
+    # Django implements "ON DELETE CASCADE" in Python, not in the database. This causes "InternalError: invalid memory
+    # alloc request size 1073741824" (1GB) [1]. There is an open issue to implement it in the database [2].
+    #
+    # 1. https://code.djangoproject.com/ticket/30533
+    # 2. https://code.djangoproject.com/ticket/21961
+    connections["default"].ops.bulk_batch_size = types.MethodType(bulk_batch_size, connections["default"].ops)
 
     Collection.objects.get(pk=collection_id).delete()
 
