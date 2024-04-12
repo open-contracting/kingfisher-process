66be7bb033c22a0a3c4fa679f7235c53f8555cfa
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-04-11 23:35:48 -0400

    chore: Move views/api.py to views.py

--- process/urls.py
+++ process/urls.py
@@ -2,8 +2,8 @@ from django.conf import settings
 from django.urls import include, path
 from rest_framework import routers
 
+from process import views
 from process.api import CollectionViewSet, TreeViewSet
-from process.views import api
 
 router = routers.DefaultRouter()
 router.register(r"collections", CollectionViewSet)
@@ -11,8 +11,8 @@ router.register(r"tree", TreeViewSet, basename="tree")
 
 urlpatterns = [
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    path(f"api/{settings.API_VERSION}/create_collection", api.create_collection, name="create_collection"),
-    path(f"api/{settings.API_VERSION}/close_collection", api.close_collection, name="close_collection"),
-    path(f"api/{settings.API_VERSION}/wipe_collection", api.wipe_collection, name="wipe_collection"),
+    path(f"api/{settings.API_VERSION}/create_collection", views.create_collection, name="create_collection"),
+    path(f"api/{settings.API_VERSION}/close_collection", views.close_collection, name="close_collection"),
+    path(f"api/{settings.API_VERSION}/wipe_collection", views.wipe_collection, name="wipe_collection"),
     path(f"api/{settings.API_VERSION}/", include(router.urls)),
 ]
