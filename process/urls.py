01db3ada3cfd774463f036c1b570914b1ce0f705
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-04-17 13:12:28 -0400

    chore: Remove unused URL 144d417 from https://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/#adding-login-to-the-browsable-api

--- process/urls.py
+++ process/urls.py
@@ -10,7 +10,6 @@ router.register(r"collections", CollectionViewSet)
 router.register(r"tree", TreeViewSet, basename="tree")
 
 urlpatterns = [
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path(f"api/{settings.API_VERSION}/create_collection", views.create_collection, name="create_collection"),
     path(f"api/{settings.API_VERSION}/close_collection", views.close_collection, name="close_collection"),
     path(f"api/{settings.API_VERSION}/wipe_collection", views.wipe_collection, name="wipe_collection"),
