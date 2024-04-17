fb27ff55a6a43f358307c8cede587d265c045146
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-04-17 01:56:45 -0400

    fix: Publish collection_closed in closecollection command, like in API, to trigger finisher if all messages processed

--- process/management/commands/closecollection.py
+++ process/management/commands/closecollection.py
@@ -1,9 +1,11 @@
 from django.core.management.base import CommandError
+from django.db import transaction
 from django.db.models.functions import Now
 from django.utils.translation import gettext as t
 from django.utils.translation import gettext_lazy as _
 
 from process.cli import CollectionCommand
+from process.util import get_publisher
 from process.util import wrap as w
 
 
@@ -26,11 +28,24 @@ class Command(CollectionCommand):
 
         self.stderr.write("Working... ", ending="")
 
-        collection.store_end_at = Now()
-        collection.save(update_fields=["store_end_at"])
+        with transaction.atomic():
+            collection.store_end_at = Now()
+            collection.save(update_fields=["store_end_at"])
 
-        if upgraded_collection := collection.get_upgraded_collection():
-            upgraded_collection.store_end_at = Now()
-            upgraded_collection.save(update_fields=["store_end_at"])
+            if upgraded_collection := collection.get_upgraded_collection():
+                upgraded_collection.store_end_at = Now()
+                upgraded_collection.save(update_fields=["store_end_at"])
+
+        with get_publisher() as client:
+            message = {"collection_id": collection.pk}
+            client.publish(message, routing_key="collection_closed")
+
+            if upgraded_collection:
+                message = {"collection_id": upgraded_collection.pk}
+                client.publish(message, routing_key="collection_closed")
+
+            if compiled_collection := collection.get_compiled_collection():
+                message = {"collection_id": compiled_collection.pk}
+                client.publish(message, routing_key="collection_closed")
 
         self.stderr.write(self.style.SUCCESS("done"))
