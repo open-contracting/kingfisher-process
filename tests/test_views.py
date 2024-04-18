588131f344abb2bc59bade5a357d7383231b3ef4
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2024-04-18 17:07:24 -0400

    test: add more views tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -44,3 +44,29 @@ class CollectionViewTests(APITestCase):
                 "published_to": "2019-11-25T17:20:44.9244234Z",
             },
         )
+
+    def test_new_400(self):
+        response = self.client.post(f"{base_url}new/?format=json", {})
+        self.assertEqual(response.status_code, 400)
+
+    def test_new_ok(self):
+        response = self.client.post(
+            f"{base_url}new/?format=json", {"source_id": "test", "data_version": "2024-04-18 00:00:00"}
+        )
+        self.assertEqual(response.status_code, 200)
+
+        self.assertJSONEqual(response.content, {"collection_id": 4})
+
+        response = self.client.post(
+            f"{base_url}new/?format=json",
+            {"source_id": "test2", "data_version": "2024-04-18 00:00:00", "upgrade": True, "compile": True},
+        )
+        self.assertEqual(response.status_code, 200)
+
+        self.assertJSONEqual(
+            response.content, {"collection_id": 5, "upgraded_collection_id": 6, "compiled_collection_id": 7}
+        )
+
+    def test_close_400(self):
+        response = self.client.post(f"{base_url}close/?format=json", {})
+        self.assertEqual(response.status_code, 400)
