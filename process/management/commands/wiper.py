16e18146043d9b1c6484cc50c372449c280d471a
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-07-03 13:50:56 -0400

    perf: Use raw queries since previous solution didn't prevent all memory from being consumed

--- process/management/commands/wiper.py
+++ process/management/commands/wiper.py
@@ -1,8 +1,8 @@
 import logging
-import types
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
-from django.db import connections
+from django.db import connection
 from django.utils.translation import gettext as t
 from yapw.methods import ack
 
@@ -13,10 +13,6 @@ from process.util import wrap as w
 consume_routing_keys = ["wiper"]
 routing_key = "wiper"
 logger = logging.getLogger(__name__)
-
-
-def bulk_batch_size(self, fields, objs):
-    return 65536  # 2**16
 
 
 class Command(BaseCommand):
@@ -31,12 +27,31 @@ class Command(BaseCommand):
 def callback(client_state, channel, method, properties, input_message):
     collection_id = input_message["collection_id"]
 
-    # Django implements "ON DELETE CASCADE" in Python, not in the database. This causes "InternalError: invalid memory
-    # alloc request size 1073741824" (1GB) [1]. There is an open issue to implement it in the database [2].
-    #
-    # 1. https://code.djangoproject.com/ticket/30533
-    # 2. https://code.djangoproject.com/ticket/21961
-    connections["default"].ops.bulk_batch_size = types.MethodType(bulk_batch_size, connections["default"].ops)
+    tables = [
+        ("record", None),  # references collection_file_item
+        ("release", None),  # references collection_file_item
+        ("compiled_release", None),  # references collection_file_item
+        ("collection_file_item", "collection_file"),  # references collection_file
+        ("processing_step", None),  # references collection_file
+        ("collection_file", None),
+        ("collection_note", None),
+    ]
+
+    if settings.ENABLE_CHECKER:
+        tables = [("record_check", "record"), ("release_check", "release")] + tables
+
+    with connection.cursor() as cursor:
+        for table, related in tables:
+            if related:
+                cursor.execute(
+                    f"DELETE FROM {table} WHERE {related}_id IN (SELECT id FROM {related} WHERE collection_id = %s)",
+                    [collection_id],
+                )
+            else:
+                cursor.execute(
+                    f"DELETE FROM {table} WHERE collection_id = %s",
+                    [collection_id],
+                )
 
     Collection.objects.filter(pk=collection_id).delete()
 
