7ae071987aeb436db80c5c5f82657ac5b3e1e95c
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-07-03 15:50:22 -0400

    fix: Use correct keyword argument in previous commit

--- process/management/commands/file_worker.py
+++ process/management/commands/file_worker.py
@@ -175,7 +175,9 @@ def process_file(collection_file) -> int | None:
 def _get_data_type(collection_file):
     collection = collection_file.collection
     if not collection.data_type:
-        detected_format, is_concatenated, is_array = detect_format(collection_file.filename, prefixes=("extensions",))
+        detected_format, is_concatenated, is_array = detect_format(
+            collection_file.filename, additional_prefixes=("extensions",)
+        )
         data_type = {
             "format": detected_format,
             "concatenated": is_concatenated,
