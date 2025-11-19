20a0a3ce7a6ee86a1cbd822c6c0493b7eb14eb02
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-11-19 18:54:43 -0500

    fix: Disable DRF authentication
    The requests package automatically sends an Authentication header if a ~/.netrc file is present -
    which can be the case in development. In that case, authentication fails and 403 Forbidden is returned,
    even though the API requires no authentication.

--- core/settings.py
+++ core/settings.py
@@ -229,6 +229,7 @@ CORS_EXPOSE_HEADERS = ["Content-Disposition"]
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": [],
 }
 
 
