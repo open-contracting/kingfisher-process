open-contracting/kingfisher-process

commit e8861c28d91f940828cef4f8de626f400935284d

fix: Add support for compiled releases

process/management/commands/file_worker.py
@@ -6,18 +6,19 @@ import simplejson as json
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import transaction
-from django.db.utils import IntegrityError
 from ijson.common import ObjectBuilder
 from ocdskit.exceptions import UnknownFormatError
 from ocdskit.upgrade import upgrade_10_11
 from ocdskit.util import detect_format
 from yapw.methods import ack, nack, publish
 
+from process import util
 from process.exceptions import UnsupportedFormatError
 from process.models import (
     CollectionFile,
     CollectionFileItem,
     CollectionNote,
+    CompiledRelease,
     Data,
     PackageData,
     ProcessingStep,
@@ -25,6 +26,7 @@ from process.models import (
     Release,
 )
 from process.util import (
+    COMPILED_RELEASE,
     RECORD_PACKAGE,
     RELEASE_PACKAGE,
     consume,
@@ -32,14 +34,14 @@ from process.util import (
     create_step,
     decorator,
     delete_step,
-    get_hash,
+    get_or_create,
 )
 
 consume_routing_keys = ["loader", "api_loader"]
 routing_key = "file_worker"
 logger = logging.getLogger(__name__)
 
-SUPPORTED_FORMATS = [RELEASE_PACKAGE, RECORD_PACKAGE]
+SUPPORTED_FORMATS = {COMPILED_RELEASE, RECORD_PACKAGE, RELEASE_PACKAGE}
 
 
 class Command(BaseCommand):
@@ -109,7 +111,10 @@ def process_file(collection_file):
             f"Unsupported data type '{data_type}' for file {collection_file}. Must be one of {SUPPORTED_FORMATS}."
         )
 
-    package = _read_package_data_from_file(collection_file.filename, data_type)
+    if data_type["format"] == COMPILED_RELEASE:
+        package = None
+    else:
+        package = _read_package_data_from_file(collection_file.filename, data_type)
 
     logger.debug("Writing data for collection_file %s", collection_file.pk)
     releases_or_records = _read_data_from_file(collection_file.filename, data_type)
@@ -162,30 +167,32 @@ class ControlCodesFilter:
 
 
 def _get_data_key(data_type):
-    data_key = ""
+    data_key = []
 
     if data_type["array"]:
-        data_key = "item."
+        data_key.append("item")
 
-    if data_type["format"] == RECORD_PACKAGE:
-        return f"{data_key}records"
+    match data_type["format"]:
+        case util.RECORD_PACKAGE:
+            data_key.extend(["records", "item"])
+        case util.RELEASE_PACKAGE:
+            data_key.extend(["releases", "item"])
 
-    if data_type["format"] == RELEASE_PACKAGE:
-        return f"{data_key}releases"
+    return ".".join(data_key)
 
 
 def _read_package_data_from_file(filename, data_type):
-    package = None
+    package = {}
 
     package_key = "item" if data_type["array"] else ""
-    data_key = _get_data_key(data_type)
+    data_key = _get_data_key(data_type).removesuffix(".item")
 
     with open(filename, "rb") as f:
         build = False
         builder = ObjectBuilder()
 
         # Constructs Decimal values. https://github.com/ICRAR/ijson#options
-        for prefix, event, value in ijson.parse(ControlCodesFilter(f)):
+        for prefix, event, value in ijson.parse(ControlCodesFilter(f), multiple_values=True):
             if prefix == package_key:
                 # Start of package.
                 if event == "start_map":
@@ -194,7 +201,7 @@ def _read_package_data_from_file(filename, data_type):
                 # End of package.
                 elif event == "end_map":
                     build = False
-                    package = builder.value
+                    return builder.value
 
             if build and not prefix.startswith(data_key):
                 builder.event(event, value)
@@ -210,8 +217,8 @@ def _read_data_from_file(filename, data_type):
         builder = ObjectBuilder()
 
         # Constructs Decimal values. https://github.com/ICRAR/ijson#options
-        for prefix, event, value in ijson.parse(ControlCodesFilter(f)):
-            if prefix == f"{data_key}.item":
+        for prefix, event, value in ijson.parse(ControlCodesFilter(f), multiple_values=True):
+            if prefix == data_key:
                 # Start of an item of data.
                 if event == "start_map":
                     build = True
@@ -229,8 +236,6 @@ def _store_data(collection_file, package, releases_or_records, data_type, upgrad
     collection_file_item = CollectionFileItem(collection_file=collection_file, number=0)
     collection_file_item.save()
 
-    package_data = _store_deduplicated_data(PackageData, package)
-
     for release_or_record in releases_or_records:
         if upgrade:
             # upgrade_10_11() requires an OrderedDict. simplejson is used for native decimal support.
@@ -238,43 +243,35 @@ def _store_data(collection_file, package, releases_or_records, data_type, upgrad
                 json.loads(json.dumps(release_or_record, use_decimal=True), object_pairs_hook=OrderedDict)
             )
 
-        data = _store_deduplicated_data(Data, release_or_record)
+        data = get_or_create(Data, release_or_record)
 
         # The ocid is required to find all the releases relating to the same record, during compilation.
         if "ocid" not in release_or_record:
             logger.error("Skipped release or record without ocid: %s", release_or_record)
             continue
 
-        if data_type["format"] == RECORD_PACKAGE:
-            Record(
-                collection=collection_file.collection,
-                collection_file_item=collection_file_item,
-                package_data=package_data,
-                data=data,
-                ocid=release_or_record["ocid"],
-            ).save()
-        elif data_type["format"] == RELEASE_PACKAGE:
-            Release(
-                collection=collection_file.collection,
-                collection_file_item=collection_file_item,
-                package_data=package_data,
-                data=data,
-                ocid=release_or_record["ocid"],
-                release_id=release_or_record["id"],
-            ).save()
-
-
-def _store_deduplicated_data(model, data):
-    hash_md5 = get_hash(data)
-
-    try:
-        obj = model.objects.get(hash_md5=hash_md5)
-    except (model.DoesNotExist, model.MultipleObjectsReturned):
-        obj = model(data=data, hash_md5=hash_md5)
-        try:
-            with transaction.atomic():
-                obj.save()
-        except IntegrityError:
-            obj = model.objects.get(hash_md5=hash_md5)
-
-    return obj
+        match data_type["format"]:
+            case util.RECORD_PACKAGE:
+                Record(
+                    collection=collection_file.collection,
+                    collection_file_item=collection_file_item,
+                    package_data=get_or_create(PackageData, package),
+                    data=data,
+                    ocid=release_or_record["ocid"],
+                ).save()
+            case util.RELEASE_PACKAGE:
+                Release(
+                    collection=collection_file.collection,
+                    collection_file_item=collection_file_item,
+                    package_data=get_or_create(PackageData, package),
+                    data=data,
+                    ocid=release_or_record["ocid"],
+                    release_id=release_or_record["id"],
+                ).save()
+            case util.COMPILED_RELEASE:
+                CompiledRelease(
+                    collection=collection_file.collection,
+                    collection_file_item=collection_file_item,
+                    data=data,
+                    ocid=release_or_record["ocid"],
+                ).save()

process/util.py
@@ -6,7 +6,7 @@ from textwrap import fill
 
 import simplejson as json
 from django.conf import settings
-from django.db import connections
+from django.db import connections, transaction
 from django.db.utils import IntegrityError
 from yapw.clients import AsyncConsumer, Blocking
 from yapw.decorators import decorate
@@ -20,6 +20,7 @@ logger = logging.getLogger(__name__)
 # These must match the output of ocdskit.util.detect_format().
 RELEASE_PACKAGE = "release package"
 RECORD_PACKAGE = "record package"
+COMPILED_RELEASE = "compiled release"
 YAPW_KWARGS = {"url": settings.RABBIT_URL, "exchange": settings.RABBIT_EXCHANGE_NAME, "prefetch_count": 20}
 
 
@@ -39,12 +40,6 @@ def walk(paths):
                 for name in files:
                     if not name.startswith("."):
                         yield os.path.join(root, name)
-
-
-def get_hash(data):
-    return hashlib.md5(
-        json.dumps(data, separators=(",", ":"), sort_keys=True, use_decimal=True).encode("utf-8")
-    ).hexdigest()
 
 
 @contextmanager
@@ -101,6 +96,24 @@ def decorator(decode, callback, state, channel, method, properties, body):
             conn.close()
 
     decorate(decode, callback, state, channel, method, properties, body, errback, finalback)
+
+
+def get_or_create(model, data):
+    hash_md5 = hashlib.md5(
+        json.dumps(data, separators=(",", ":"), sort_keys=True, use_decimal=True).encode("utf-8")
+    ).hexdigest()
+
+    try:
+        obj = model.objects.get(hash_md5=hash_md5)
+    except (model.DoesNotExist, model.MultipleObjectsReturned):
+        obj = model(data=data, hash_md5=hash_md5)
+        try:
+            with transaction.atomic():
+                obj.save()
+        except IntegrityError:
+            obj = model.objects.get(hash_md5=hash_md5)
+
+    return obj
 
 
 def create_note(collection, code, note, **kwargs):

process/processors/compiler.py
@@ -9,7 +9,7 @@ from ocdsextensionregistry.exceptions import ExtensionWarning
 from ocdsextensionregistry.profile_builder import ProfileBuilder
 
 from process.models import CollectionFile, CollectionFileItem, CollectionNote, CompiledRelease, Data
-from process.util import create_note, get_hash
+from process.util import create_note, get_or_create
 
 logger = logging.getLogger(__name__)
 
@@ -21,13 +21,7 @@ def save_compiled_release(merged, collection, ocid):
     collection_file_item = CollectionFileItem(collection_file=collection_file, number=0)
     collection_file_item.save()
 
-    hash_md5 = get_hash(merged)
-
-    try:
-        data = Data.objects.get(hash_md5=hash_md5)
-    except (Data.DoesNotExist, Data.MultipleObjectsReturned):
-        data = Data(data=merged, hash_md5=hash_md5)
-        data.save()
+    data = get_or_create(Data, merged)
 
     release = CompiledRelease(collection=collection, collection_file_item=collection_file_item, data=data, ocid=ocid)
     release.save()

commit f53c7789be689d5f5581da5d7788a137fe13ac1e

perf: Reduce queries in file_worker. build: Upgrade OCDS Kit to simplify upgrade call.
- Create the package_data row once (a regression in e8861c2 compared to dc33a56)
- Bulk create data rows if DEDUPLICATE_DATA is disabled

core/settings.py
@@ -237,9 +237,6 @@ REST_FRAMEWORK = {
 
 # Project configuration
 
-# The version of OCDS with which to initialize the ProfileBuilder.
-COMPILER_OCDS_VERSION = "1__1__5"
-
 # The connection string for RabbitMQ.
 RABBIT_URL = os.getenv("RABBIT_URL", "amqp://127.0.0.1")
 # The name of the RabbitMQ exchange. Follow the pattern `{project}_{service}_{environment}`.
@@ -259,7 +256,14 @@ KINGFISHER_COLLECT_FILES_STORE = os.getenv(
     "KINGFISHER_COLLECT_FILES_STORE", "/data" if production else BASE_DIR / "data"
 )
 
-ENABLE_CHECKER = "ENABLE_CHECKER" in os.environ
-
 # Whether to deduplicate rows in the `package_data` and `data` tables.
 DEDUPLICATE_DATA = os.getenv("DEDUPLICATE_DATA", "True") == "True"
+
+# The number of rows to insert per statement, when DEDUPLICATE_DATA is disabled.
+BULK_CREATE_BATCH_SIZE = int(os.getenv("BULK_CREATE_BATCH_SIZE", "1000"))
+
+# The version of OCDS with which to initialize the ProfileBuilder.
+COMPILER_OCDS_VERSION = "1__1__5"
+
+# Whether to enable the `checker` worker.
+ENABLE_CHECKER = "ENABLE_CHECKER" in os.environ

process/management/commands/file_worker.py
@@ -2,9 +2,9 @@ import logging
 import random
 import time
 from collections import OrderedDict
+from itertools import islice
 
 import ijson
-import simplejson as json
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import OperationalError, transaction
@@ -305,46 +305,67 @@ def _read_data_from_file(filename, data_type):
 
 
 def _store_data(collection_file, package, releases_or_records, data_type, *, upgrade=False):
-    for release_or_record in releases_or_records:
+    collection = collection_file.collection
+    package_data = get_or_create(PackageData, package) if package is not None else None
+
+    # With deduplication, each Data row is looked up or created individually (batch size 1).
+    batch_size = 1 if settings.DEDUPLICATE_DATA else settings.BULK_CREATE_BATCH_SIZE
+
+    while batched_releases_or_records := list(islice(releases_or_records, batch_size)):
         if upgrade:
-            with create_logger_note(collection_file.collection, "ocdskit"):
-                # upgrade_10_11() requires an OrderedDict. simplejson is used for native decimal support.
-                # This requirement can be removed: https://github.com/open-contracting/ocdskit/issues/164
-                release_or_record = upgrade_10_11(
-                    json.loads(json.dumps(release_or_record, use_decimal=True), object_pairs_hook=OrderedDict)
-                )
+            upgraded_releases_or_records = []
+            for release_or_record in batched_releases_or_records:
+                with create_logger_note(collection, "ocdskit"):
+                    upgraded_releases_or_records.append(upgrade_10_11(release_or_record, reorder=False))
+            batched_releases_or_records = upgraded_releases_or_records
 
-        data = get_or_create(Data, release_or_record)
+        if settings.DEDUPLICATE_DATA:
+            data_objects = [get_or_create(Data, data) for data in batched_releases_or_records]
+        else:
+            data_objects = [Data(hash_md5="", data=data) for data in batched_releases_or_records]
+            Data.objects.bulk_create(data_objects)
 
-        # The ocid is required to find all the releases relating to the same record, during compilation.
-        if "ocid" not in release_or_record:
-            logger.error("Skipped release or record without ocid: %s", release_or_record)
-            continue
+        rows = []
+        for release_or_record, data in zip(batched_releases_or_records, data_objects, strict=True):
+            # The ocid is required to find all the releases relating to the same record, during compilation.
+            if "ocid" not in release_or_record:
+                logger.error("Skipped release or record without ocid: %s", release_or_record)
+                continue
 
-        match data_type["format"]:
-            case Format.record_package:
-                Record(
-                    collection=collection_file.collection,
-                    collection_file=collection_file,
-                    package_data=get_or_create(PackageData, package),
-                    data=data,
-                    ocid=release_or_record["ocid"],
-                ).save()
-            case Format.release_package:
-                Release(
-                    collection=collection_file.collection,
-                    collection_file=collection_file,
-                    package_data=get_or_create(PackageData, package),
-                    data=data,
-                    ocid=release_or_record["ocid"],
-                    release_id=release_or_record.get("id") or "",
-                    release_date=release_or_record.get("date") or "",
-                ).save()
-            case Format.compiled_release:
-                CompiledRelease(
-                    collection=collection_file.collection,
-                    collection_file=collection_file,
-                    data=data,
-                    ocid=release_or_record["ocid"],
-                    release_date=release_or_record.get("date") or "",
-                ).save()
+            match data_type["format"]:
+                case Format.record_package:
+                    rows.append(
+                        Record(
+                            collection=collection,
+                            collection_file=collection_file,
+                            package_data=package_data,
+                            data=data,
+                            ocid=release_or_record["ocid"],
+                        )
+                    )
+                case Format.release_package:
+                    rows.append(
+                        Release(
+                            collection=collection,
+                            collection_file=collection_file,
+                            package_data=package_data,
+                            data=data,
+                            ocid=release_or_record["ocid"],
+                            release_id=release_or_record.get("id") or "",
+                            release_date=release_or_record.get("date") or "",
+                        )
+                    )
+                case Format.compiled_release:
+                    rows.append(
+                        CompiledRelease(
+                            collection=collection,
+                            collection_file=collection_file,
+                            data=data,
+                            ocid=release_or_record["ocid"],
+                            release_date=release_or_record.get("date") or "",
+                        )
+                    )
+
+        if rows:
+            # A collection has a single format.
+            type(rows[0]).objects.bulk_create(rows)

process/util.py
@@ -91,7 +91,7 @@ def decorator(decode, callback, state, channel, method, properties, body):
 
 
 def get_or_create(model, data):
-    """Get or create a Data or PackageData instance."""
+    """Get or create a PackageData or Data instance."""
     if not settings.DEDUPLICATE_DATA:
         return model.objects.create(hash_md5="", data=data)
 

tests/processors/test_process_file.py
@@ -1,9 +1,10 @@
-from django.test import TransactionTestCase
+from django.test import TransactionTestCase, override_settings
 from ocdskit.exceptions import UnknownFormatError
+from ocdskit.util import Format
 
 from process.exceptions import EmptyFormatError, UnsupportedFormatError
 from process.management.commands.file_worker import process_file, set_data_type
-from process.models import CollectionFile, PackageData, Release
+from process.models import CollectionFile, CompiledRelease, Data, PackageData, Record, Release
 from tests.fixtures import collection
 
 
@@ -94,3 +95,72 @@ class ProcessFileTests(TransactionTestCase):
         self.assertEqual(upgraded_collection_file.filename, "tests/fixtures/collection_file.json")
         self.assertEqual(upgraded_collection_file.filename, "tests/fixtures/collection_file.json")
         self.assertEqual(upgraded_collection_file.filename, "tests/fixtures/collection_file.json")
+
+
+@override_settings(DEDUPLICATE_DATA=False)
+class ProcessFileWithoutDeduplicationTests(TransactionTestCase):
+    def test_bulk_store_release_package(self):
+        for index, batch_size in enumerate((1, 30, 1000)):
+            with self.subTest(batch_size=batch_size), override_settings(BULK_CREATE_BATCH_SIZE=batch_size):
+                source = collection()
+                source.data_version = f"2001-01-0{index + 1} 00:00:00"
+                source.data_type = {"format": Format.release_package, "concatenated": False, "array": True}
+                source.save()
+
+                collection_file = CollectionFile(collection=source, filename="tests/fixtures/collection_file.json")
+                collection_file.save()
+
+                process_file(collection_file)
+
+                releases = Release.objects.filter(collection_file=collection_file).select_related("data")
+                package_data_ids = {release.package_data_id for release in releases}
+                data_ids = {release.data_id for release in releases}
+
+                self.assertEqual(releases.count(), 100)
+                self.assertEqual(len(package_data_ids), 1)
+                self.assertEqual(len(data_ids), 100)
+                self.assertTrue(len({release.data.data["ocid"] for release in releases}), 100)
+
+                package_data = PackageData.objects.get(pk=next(iter(package_data_ids)))
+
+                self.assertEqual(releases.filter(package_data=package_data).count(), 100)
+                self.assertNotIn("releases", package_data.data)
+
+    def test_bulk_store_record_package(self):
+        source = collection()
+        source.data_type = {"format": Format.record_package, "concatenated": False, "array": True}
+        source.save()
+
+        collection_file = CollectionFile(collection=source, filename="tests/fixtures/record_package.json")
+        collection_file.save()
+
+        process_file(collection_file)
+
+        records = Record.objects.filter(collection_file=collection_file)
+
+        self.assertEqual(records.count(), 2)
+        self.assertEqual(PackageData.objects.count(), 1)
+        self.assertEqual(Data.objects.count(), 2)
+        self.assertEqual(set(records.values_list("ocid", flat=True)), {"ocds-aaa111", "ocds-bbb222"})
+
+        package_data = PackageData.objects.get()
+
+        self.assertEqual(records.filter(package_data=package_data).count(), 2)
+        self.assertNotIn("records", package_data.data)
+
+    def test_bulk_store_compiled_release(self):
+        source = collection()
+        source.data_type = {"format": Format.compiled_release, "concatenated": True, "array": False}
+        source.save()
+
+        collection_file = CollectionFile(collection=source, filename="tests/fixtures/compiled_release.json")
+        collection_file.save()
+
+        process_file(collection_file)
+
+        compiled_releases = CompiledRelease.objects.filter(collection_file=collection_file)
+
+        self.assertEqual(compiled_releases.count(), 2)
+        self.assertEqual(PackageData.objects.count(), 0)
+        self.assertEqual(Data.objects.count(), 2)
+        self.assertEqual(set(compiled_releases.values_list("ocid", flat=True)), {"ocds-aaa111", "ocds-bbb222"})

tests/test_util.py
@@ -24,9 +24,10 @@ class UtilTests(SimpleTestCase):
         create_note.assert_called_once_with(
             "collection",
             CollectionNote.Level.WARNING,
-            'party in "supplier" role differs from party in ["tenderer"] roles:\n{"name": "Acme Inc.", "identifier": '
-            '{"id": 1}, "additionalIdentifiers": [{"id": "a"}], "id": "3c9756cf8983b14066a034079aa7aae4"}\n{"id": '
-            '"3c9756cf8983b14066a034079aa7aae4", "name": "Acme Inc.", "identifier": {"id": 1}}\n',
+            'party in "supplier" role differs from party in ["tenderer"] roles:\n'
+            '{"id": "3c9756cf8983b14066a034079aa7aae4", "name": "Acme Inc.", "identifier": {"id": 1}, '
+            '"additionalIdentifiers": [{"id": "a"}]}\n'
+            '{"id": "3c9756cf8983b14066a034079aa7aae4", "name": "Acme Inc.", "identifier": {"id": 1}}\n',
         )
 
     @patch("process.util.create_note")